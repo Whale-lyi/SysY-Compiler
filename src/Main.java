d8c60feaf28e68d6e0c41b433fd3d6bcd7e54f17
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-07 23:19:06 +0800

    > make 201250141 whale Linux whale-virtual-machine 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:19:06 up  1:36,  1 user,  load average: 1.24, 1.41, 1.06 1598c6a8bbbd65e6e77c32c570152c8f731b0bb

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.io.*;
+
 public class Main
 {    
     public static void main(String[] args){
-        System.out.println(args.length);
-        for (String s : args) {
-            System.out.println(s);
+        String filePath = args[0];
+        try {
+            File file = new File(filePath);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }

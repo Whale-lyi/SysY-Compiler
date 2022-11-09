ed2138188b1607585c6a569e7a6611be3404d1e7
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-10 00:46:04 +0800

    > make 201250141 whale Linux whale-virtual-machine 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:46:04 up  4:05,  1 user,  load average: 0.02, 0.12, 0.33 b6d7b3f771ba9fcbe531d1d824df4cad35990bb3

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,6 @@ public class Main
             System.err.println("input path is required");
         }
         String source = args[0];
-//        String source = "hello.txt";
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
         sysYLexer.removeErrorListeners();

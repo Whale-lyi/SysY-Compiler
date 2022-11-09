86dc40f1c231b1a1078241e3c5a8dc2bea4cc64a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-09 23:30:43 +0800

    > make 201250141 whale Linux whale-virtual-machine 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:30:43 up  2:50,  1 user,  load average: 0.79, 0.49, 0.42 682eb06b4ce38a576a3824d299d7d0d266919dbb

--- src/Main.java
+++ src/Main.java
@@ -18,16 +18,18 @@ public class Main
         sysYLexer.removeErrorListeners();
         sysYLexer.addErrorListener(new MyErrorListener());
         List<? extends Token> allTokens = sysYLexer.getAllTokens();
-        String[] ruleNames = sysYLexer.getRuleNames();
-        for (Token token : allTokens) {
-            String ruleName = ruleNames[token.getType() - 1];
-            if (ruleName.equals("INTEGR_CONST")) {
-                Integer number = parseInt(token.getText());
-                System.err.println(ruleName + " " + number + " at Line " + token.getLine() + ".");
-            } else {
-                System.err.println(ruleName + " " + token.getText() + " at Line " + token.getLine() + ".");
+        try {
+            String[] ruleNames = sysYLexer.getRuleNames();
+            for (Token token : allTokens) {
+                String ruleName = ruleNames[token.getType() - 1];
+                if (ruleName.equals("INTEGR_CONST")) {
+                    Integer number = parseInt(token.getText());
+                    System.err.println(ruleName + " " + number + " at Line " + token.getLine() + ".");
+                } else {
+                    System.err.println(ruleName + " " + token.getText() + " at Line " + token.getLine() + ".");
+                }
             }
-        }
+        } catch (RuntimeException ignored) {}
     }
 
     public static Integer parseInt(String text) {

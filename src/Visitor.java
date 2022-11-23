c03a2a35ea9c6386a48d474cec210537011cbdfd
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-23 18:53:39 +0800

    > make 201250141 whale Linux whale-virtual-machine 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:53:39 up  2:41,  1 user,  load average: 0.41, 0.54, 0.48 7c020951e22159ab1345485387bd525eb0f95b

--- src/Visitor.java
+++ src/Visitor.java
@@ -4,13 +4,13 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 public class Visitor extends SysYParserBaseVisitor<Void>{
     @Override
     public Void visitChildren(RuleNode node) {
-        System.out.println("visitChildren: " + node.getRuleContext().getText());
+        System.out.println("visitChildren: " + node.getRuleContext().toString());
         return super.visitChildren(node);
     }
 
     @Override
     public Void visitTerminal(TerminalNode node) {
-        System.out.println("visitTerminal: " + node.getSymbol().getText());
+//        System.out.println("visitTerminal: " + node.getSymbol().getText());
         return super.visitTerminal(node);
     }
 }

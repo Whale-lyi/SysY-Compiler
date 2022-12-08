b5d843b473cdc1a840d443206d0f2c9b1af49377
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-12-08 18:30:17 +0800

    > make 201250141 whale Linux whale-virtual-machine 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:30:17 up  1:23,  1 user,  load average: 2.05, 1.16, 0.68 c236f1873f521d76d82f56725e074a0629d61950

--- src/visitor/Visitor.java
+++ src/visitor/Visitor.java
@@ -1,69 +1,69 @@
-package visitor;
-
-import org.antlr.v4.runtime.RuleContext;
-import org.antlr.v4.runtime.tree.RuleNode;
-import org.antlr.v4.runtime.tree.TerminalNode;
-
-public class Visitor extends SysYParserBaseVisitor<Void>{
-
-    private final String[] parserRuleNames;
-    private final String[] lexerRuleNames;
-    private final String[] highLight;
-
-    public Visitor(String[] parserRuleNames, String[] lexerRuleNames) {
-        this.parserRuleNames = parserRuleNames;
-        this.lexerRuleNames = lexerRuleNames;
-        this.highLight = new String[lexerRuleNames.length];
-        for (int i = 0; i < highLight.length; i++) {
-            if (i + 1 <= 9) {
-                highLight[i] = "[orange]";
-            } else if (i + 1 <= 24) {
-                highLight[i] = "[blue]";
-            } else if (i + 1 == 33) {
-                highLight[i] = "[red]";
-            } else if (i + 1 == 34) {
-                highLight[i] = "[green]";
-            }
-        }
-    }
-
-    @Override
-    public Void visitChildren(RuleNode node) {
-        int depth = node.getRuleContext().depth();
-        String ruleName = parserRuleNames[node.getRuleContext().getRuleIndex()];
-        System.err.println(getIndent(depth) + titleCase(ruleName));
-        return super.visitChildren(node);
-    }
-
-    @Override
-    public Void visitTerminal(TerminalNode node) {
-        RuleContext ruleContext = (RuleContext) node.getParent();
-        int depth = ruleContext.depth() + 1;
-        int type = node.getSymbol().getType();
-        if ((type >= 1 && type <= 24) || type == 33) {
-            String ruleName = lexerRuleNames[type - 1];
-            System.err.println(getIndent(depth) + node.getSymbol().getText() + " " + ruleName + highLight[type - 1]);
-        } else if (type == 34) {
-            String ruleName = lexerRuleNames[type - 1];
-            System.err.println(getIndent(depth) + parseInt(node.getSymbol().getText()) + " " + ruleName + highLight[type - 1]);
-        }
-        return super.visitTerminal(node);
-    }
-
-    public static String getIndent(int depth) {
-        return "  ".repeat(Math.max(0, depth - 1));
-    }
-
-    public static String titleCase(String ruleName) {
-        return ruleName.substring(0, 1).toUpperCase() + ruleName.substring(1);
-    }
-
-    public static Integer parseInt(String text) {
-        if (text.startsWith("0x") || text.startsWith("0X")) {
-            return Integer.parseInt(text.substring(2), 16);
-        } else if (text.startsWith("0") && text.length() > 1) {
-            return Integer.parseInt(text.substring(1), 8);
-        }
-        return Integer.parseInt(text);
-    }
-}
+//package visitor;
+//
+//import org.antlr.v4.runtime.RuleContext;
+//import org.antlr.v4.runtime.tree.RuleNode;
+//import org.antlr.v4.runtime.tree.TerminalNode;
+//
+//public class Visitor extends SysYParserBaseVisitor<Void>{
+//
+//    private final String[] parserRuleNames;
+//    private final String[] lexerRuleNames;
+//    private final String[] highLight;
+//
+//    public Visitor(String[] parserRuleNames, String[] lexerRuleNames) {
+//        this.parserRuleNames = parserRuleNames;
+//        this.lexerRuleNames = lexerRuleNames;
+//        this.highLight = new String[lexerRuleNames.length];
+//        for (int i = 0; i < highLight.length; i++) {
+//            if (i + 1 <= 9) {
+//                highLight[i] = "[orange]";
+//            } else if (i + 1 <= 24) {
+//                highLight[i] = "[blue]";
+//            } else if (i + 1 == 33) {
+//                highLight[i] = "[red]";
+//            } else if (i + 1 == 34) {
+//                highLight[i] = "[green]";
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public Void visitChildren(RuleNode node) {
+//        int depth = node.getRuleContext().depth();
+//        String ruleName = parserRuleNames[node.getRuleContext().getRuleIndex()];
+//        System.err.println(getIndent(depth) + titleCase(ruleName));
+//        return super.visitChildren(node);
+//    }
+//
+//    @Override
+//    public Void visitTerminal(TerminalNode node) {
+//        RuleContext ruleContext = (RuleContext) node.getParent();
+//        int depth = ruleContext.depth() + 1;
+//        int type = node.getSymbol().getType();
+//        if ((type >= 1 && type <= 24) || type == 33) {
+//            String ruleName = lexerRuleNames[type - 1];
+//            System.err.println(getIndent(depth) + node.getSymbol().getText() + " " + ruleName + highLight[type - 1]);
+//        } else if (type == 34) {
+//            String ruleName = lexerRuleNames[type - 1];
+//            System.err.println(getIndent(depth) + parseInt(node.getSymbol().getText()) + " " + ruleName + highLight[type - 1]);
+//        }
+//        return super.visitTerminal(node);
+//    }
+//
+//    public static String getIndent(int depth) {
+//        return "  ".repeat(Math.max(0, depth - 1));
+//    }
+//
+//    public static String titleCase(String ruleName) {
+//        return ruleName.substring(0, 1).toUpperCase() + ruleName.substring(1);
+//    }
+//
+//    public static Integer parseInt(String text) {
+//        if (text.startsWith("0x") || text.startsWith("0X")) {
+//            return Integer.parseInt(text.substring(2), 16);
+//        } else if (text.startsWith("0") && text.length() > 1) {
+//            return Integer.parseInt(text.substring(1), 8);
+//        }
+//        return Integer.parseInt(text);
+//    }
+//}

d8203fed5116aeb6bcb9fe4a82a1ce250f895e25
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-12-10 14:17:48 +0800

    > make 201250141 whale Linux whale-virtual-machine 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:17:48 up  1:13,  1 user,  load average: 0.58, 0.48, 0.37 cba5bc2e8e655fe9e61851eac8a576890b9e427

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import listener.MyParserErrorListener;
+import listener.MyTypeErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -16,6 +17,11 @@ public class Main {
         SysYLexer sysYLexer = new SysYLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
         SysYParser sysYParser = new SysYParser(tokens);
+
+        sysYParser.removeErrorListeners();
+        MyTypeErrorListener typeErrorListener = new MyTypeErrorListener();
+        sysYParser.addErrorListener(typeErrorListener);
+
         ParseTree tree = sysYParser.program();
 
         Visitor visitor = new Visitor(sysYParser.getRuleNames(), sysYLexer.getRuleNames());
---
+++ src/listener/MyTypeErrorListener.java
@@ -0,0 +1,15 @@
+package listener;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+public class MyTypeErrorListener extends BaseErrorListener {
+    public boolean used = false;
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        System.err.println("Error type B at Line " + line + ":" + msg);
+        used = true;
+    }
+}

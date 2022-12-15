58295c804a7b719bb0304392b1e00bef9c5bdf0a
liuyu <201250141@smail.nju.edu.cn>
2022-12-15 20:55:26 +0800

    dirty

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,7 @@ public class Main {
         if (args.length != 4) {
             System.err.println("There should be 4 arguments");
         }
-//        String source = args[0];
-        String source = "tests/test1.sysy";
+        String source = args[0];
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
@@ -21,14 +20,12 @@ public class Main {
         ParseTree tree = sysYParser.program();
 
         ParseTreeWalker parseTreeWalker = new ParseTreeWalker();
-//        TypeCheckingListener typeCheckingListener = new TypeCheckingListener(new Position(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-        TypeCheckingListener typeCheckingListener = new TypeCheckingListener(new Position(1, 10));
+        TypeCheckingListener typeCheckingListener = new TypeCheckingListener(new Position(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
         parseTreeWalker.walk(typeCheckingListener, tree);
 
 
         if (!typeCheckingListener.hasError) {
-//            Visitor visitor = new Visitor(sysYParser.getRuleNames(), sysYLexer.getRuleNames(), typeCheckingListener.getSymbol(), args[3]);
-            Visitor visitor = new Visitor(sysYParser.getRuleNames(), sysYLexer.getRuleNames(), typeCheckingListener.getSymbol(), "d");
+            Visitor visitor = new Visitor(sysYParser.getRuleNames(), sysYLexer.getRuleNames(), typeCheckingListener.getSymbol(), args[3]);
             visitor.visit(tree);
         }
     }

36b378a86c992773f94141215823a3a25da27920
liuyu <201250141@smail.nju.edu.cn>
2022-12-16 15:57:25 +0800

    dirty

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,8 @@ public class Main {
         if (args.length != 4) {
             System.err.println("There should be 4 arguments");
         }
-//        String source = args[0];
-        String source = "tests/test1.sysy";
+        String source = args[0];
+//        String source = "tests/test1.sysy";
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
@@ -21,8 +21,8 @@ public class Main {
         ParseTree tree = sysYParser.program();
 
         ParseTreeWalker parseTreeWalker = new ParseTreeWalker();
-//        TypeCheckingListener typeCheckingListener = new TypeCheckingListener(new Position(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-        TypeCheckingListener typeCheckingListener = new TypeCheckingListener(new Position(0, 0));
+        TypeCheckingListener typeCheckingListener = new TypeCheckingListener(new Position(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+//        TypeCheckingListener typeCheckingListener = new TypeCheckingListener(new Position(0, 0));
         parseTreeWalker.walk(typeCheckingListener, tree);
 
 

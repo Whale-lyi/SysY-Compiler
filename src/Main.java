cc7fa64535d9de9c8a2ab58449dd7b121023ce40
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-12-23 01:00:32 +0800

    > make 201250141 whale Linux whale-virtual-machine 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:00:32 up 26 min,  1 user,  load average: 0.98, 0.91, 0.84 b0f88077acf78ac4590830df7a99f221110cfe51

--- src/Main.java
+++ src/Main.java
@@ -9,25 +9,25 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        if (args.length != 4) {
-            System.err.println("There should be 4 arguments");
+        if (args.length != 2) {
+            System.err.println("There should be 2 arguments");
         }
-        String source = args[0];
-        CharStream input = CharStreams.fromFileName(source);
-        SysYLexer sysYLexer = new SysYLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
-        SysYParser sysYParser = new SysYParser(tokens);
-        ParseTree tree = sysYParser.program();
-
-        ParseTreeWalker parseTreeWalker = new ParseTreeWalker();
-        TypeCheckingListener typeCheckingListener = new TypeCheckingListener(new Position(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-        parseTreeWalker.walk(typeCheckingListener, tree);
-
-
-        if (!typeCheckingListener.hasError) {
-            Visitor visitor = new Visitor(sysYParser.getRuleNames(), sysYLexer.getRuleNames(), typeCheckingListener.getSymbol(), args[3]);
-            visitor.visit(tree);
-        }
+//        String source = args[0];
+//        CharStream input = CharStreams.fromFileName(source);
+//        SysYLexer sysYLexer = new SysYLexer(input);
+//        CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
+//        SysYParser sysYParser = new SysYParser(tokens);
+//        ParseTree tree = sysYParser.program();
+//
+//        ParseTreeWalker parseTreeWalker = new ParseTreeWalker();
+//        TypeCheckingListener typeCheckingListener = new TypeCheckingListener(new Position(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+//        parseTreeWalker.walk(typeCheckingListener, tree);
+//
+//
+//        if (!typeCheckingListener.hasError) {
+//            Visitor visitor = new Visitor(sysYParser.getRuleNames(), sysYLexer.getRuleNames(), typeCheckingListener.getSymbol(), args[3]);
+//            visitor.visit(tree);
+//        }
     }
 
 }
